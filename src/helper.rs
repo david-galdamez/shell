ba872e1e8089611206214b1acb1284e2cace9813
David Galdamez <david.galdamez2014@gmail.com>
2026-06-24 11:47:09 -0600

    feat: implemented file completion

--- src/helper.rs
+++ src/helper.rs
@@ -1,14 +1,22 @@
 use rustyline::{
-    Helper, completion::Completer, highlight::Highlighter, hint::Hinter, validate::Validator,
+    Helper,
+    completion::{Completer, FilenameCompleter, extract_word},
+    highlight::Highlighter,
+    hint::Hinter,
+    validate::Validator,
 };
 
 use crate::executables::Executables;
 
-pub struct ShellHelper;
+pub struct ShellHelper {
+    filenames: FilenameCompleter,
+}
 
 impl ShellHelper {
     pub fn default() -> Self {
-        ShellHelper
+        ShellHelper {
+            filenames: FilenameCompleter::new(),
+        }
     }
 
     // helper function, we get the executables of the path, we filter the executables by the ones
@@ -38,22 +46,33 @@ impl Completer for ShellHelper {
     type Candidate = String;
 
     fn complete(
-        &self, // FIXME should be `&mut self`
+        &self,
         line: &str,
         pos: usize,
-        _ctx: &rustyline::Context<'_>,
+        ctx: &rustyline::Context<'_>,
     ) -> rustyline::Result<(usize, Vec<Self::Candidate>)> {
+        let (word_start, prefix) = extract_word(line, pos, None, |c: char| c == ' ');
+        let is_first_word = line[..word_start].trim().is_empty();
 
-        let prefix = line;
+        if is_first_word {
+            if prefix == "ech" && pos == line.len() {
+                return Ok((0, vec!["echo".to_string()]));
+            } else if prefix == "exi" && pos == line.len() {
+                return Ok((0, vec!["exit".to_string()]));
+            }
 
-        if prefix == "ech" && pos == line.len() {
-            return Ok((0, vec!["echo".to_string()]));
-        } else if prefix == "exi" && pos == line.len() {
-            return Ok((0, vec!["exit".to_string()]));
+            let candidates = self.get_canditates(prefix)?;
+            Ok((0, candidates))
+        } else {
+            let (start, mut pairs) = self.filenames.complete(line, pos, ctx)?;
+            let mut candidates = Vec::new();
+            for pair in &mut pairs {
+                if !pair.replacement.ends_with('/') && !pair.replacement.ends_with(' ') {
+                    candidates.push(pair.replacement.clone());
+                }
+            }
+            Ok((start, candidates))
         }
-
-        let candidates = self.get_canditates(prefix)?;
-        Ok((0, candidates))
     }
 
     fn update(
@@ -64,7 +83,8 @@ impl Completer for ShellHelper {
         cl: &mut rustyline::Changeset,
     ) {
         //check if there are other candidates
-        let has_more_candidates = self.get_canditates(elected)
+        let has_more_candidates = self
+            .get_canditates(elected)
             .map(|candidate| candidate.iter().any(|c| c != elected))
             .unwrap_or(false);
 
